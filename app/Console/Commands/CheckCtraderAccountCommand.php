9378a21aff911151c80c185e1d097b26cec13aec
Wayne Lai <64002360+wayne-06@users.noreply.github.com>
2023-11-08 15:56:23 +0800

    fix: update cronjob check ctrader acc

--- app/Console/Commands/CheckCtraderAccountCommand.php
+++ app/Console/Commands/CheckCtraderAccountCommand.php
@@ -21,81 +21,82 @@ class CheckCtraderAccountCommand extends Command
         $conn = (new CTraderService)->connectionStatus();
 
         if ($conn['code'] != 0) {
-            try {
-                $users = User::query()
-                    ->whereIn('role', ['ib', 'member'])
-                    ->where('remark', 'vietnam plan')
-                    ->get();
+            return; // No need to proceed if the connection is not established
+        }
 
-                foreach ($users as $user) {
-                    (new CTraderService)->getUserInfo($user->tradingUsers);
+        try {
+            $this->updateCTraderInfo();
+        } catch (\Exception $e) {
+            \Log::error('Cronjob Updating got CTrader Error: ' . $e->getMessage());
+        }
+
+        $this->updateIbAccounts();
+    }
+
+    protected function updateCTraderInfo(): void
+    {
+        $users = User::query()
+            ->whereIn('role', ['ib', 'member'])
+            ->where('remark', 'vietnam plan')
+            ->get();
+
+        foreach ($users as $user) {
+            (new CTraderService)->getUserInfo($user->tradingUsers);
+        }
+    }
+
+    protected function updateIbAccounts(): void
+    {
+        $tradingAccounts = TradingAccount::whereHas('ofUser', function ($query) {
+            $query->where('role', 'ib');
+        })->get();
+        $ibAccounts = IbAccountType::all();
+
+        foreach ($tradingAccounts as $tradingAccount) {
+            $ibAccount = $ibAccounts
+                ->where('user_id', $tradingAccount->user_id)
+                ->where('account_type', $tradingAccount->account_type)
+                ->first();
+
+            if ($ibAccount) {
+                if ($ibAccount->account_type !== $tradingAccount->account_type) {
+                    $ibAccount->update([
+                        'account_type' => $tradingAccount->account_type,
+                    ]);
                 }
-
-                $tradingAccounts = TradingAccount::whereHas('ofUser', function ($query) {
-                    $query->where('role', 'ib');
-                })->get();
-                $ibAccounts = IbAccountType::all();
-
-                foreach ($tradingAccounts as $tradingAccount) {
-                    $ibAccount = $ibAccounts
-                        ->where('user_id', $tradingAccount->user_id)
-                        ->first();
-
-                    if ($ibAccount) {
-                        if ($ibAccount->account_type !== $tradingAccount->account_type) {
-                            $ibAccount->update([
-                                'account_type' => $tradingAccount->account_type,
-                            ]);
-                        }
-                    } else {
-                        $user = $tradingAccount->ofUser;
-                        $ib = new IbAccountType;
-                        $ib->user_id = $tradingAccount->user_id;
-                        $ib->account_type = $tradingAccount->account_type;
-
-                        if ($user->upline_id) {
-                            $ib_upline = IbAccountType::where('user_id', $user->upline_id)
-                                ->where('account_type', $tradingAccount->account_type)
-                                ->first();
-
-                            if ($ib_upline) {
-                                $ib->upline_id = $ib_upline->id;
-                                $hierarchyList = $ib_upline->hierarchyList . $ib_upline->id . "-";
-                            } else {
-                                // If $ib_upline doesn't exist, create a new IbAccountType for it
-                                $newIbUpline = new IbAccountType;
-                                $newIbUpline->user_id = $user->upline_id;
-                                $newIbUpline->account_type = $tradingAccount->account_type;
-                                $newIbUpline->save();
-
-                                $ib->upline_id = $newIbUpline->id;
-                                $hierarchyList = "-" . $newIbUpline->id . "-";
-
-                                for ($i = 1; $i <= 7; $i++) {
-                                    IbAccountTypeSymbolGroupRate::create([
-                                        'ib_account_type' => $newIbUpline->id,
-                                        'symbol_group' => $i,
-                                        'amount' => 0,
-                                    ]);
-                                }
-                            }
-                            $ib->hierarchyList = $hierarchyList;
-                        }
-
-                        $ib->save();
-
-                        for ($i = 1; $i <= 7; $i++) {
-                            IbAccountTypeSymbolGroupRate::create([
-                                'ib_account_type' => $ib->id,
-                                'symbol_group' => $i,
-                                'amount' => 0,
-                            ]);
-                        }
-                    }
-                }
-            } catch (\Exception $e) {
-                \Log::error('Cronjob Updating got CTrader Error: ' . $e->getMessage());
+            } else {
+                $this->createIbAccount($tradingAccount);
             }
+        }
+    }
+
+    protected function createIbAccount($tradingAccount): void
+    {
+        $user = $tradingAccount->ofUser;
+        $ib = new IbAccountType;
+        $ib->user_id = $tradingAccount->user_id;
+        $ib->account_type = $tradingAccount->account_type;
+
+        if ($user->upline_id) {
+            $ib_upline = IbAccountType::where('user_id', $user->upline_id)->first();
+            $ib->upline_id = $ib_upline->id;
+
+            $hierarchyList = null;
+            if (!$ib_upline->hierarchyList) {
+                $hierarchyList = "-" . $ib_upline->id . "-";
+            } else {
+                $hierarchyList = $ib_upline->hierarchyList . $ib_upline->id . "-";
+            }
+            $ib->hierarchyList = $hierarchyList;
+        }
+        $ib->save();
+
+        for ($i = 1; $i <= 7; $i++) {
+            IbAccountTypeSymbolGroupRate::create([
+                'ib_account_type' => $ib->id,
+                'symbol_group' => $i,
+                'amount' => 0,
+            ]);
         }
     }
 }
