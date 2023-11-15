f7c184269cca8fcaf4254ef61503b4c38fe62108
Wayne Lai <64002360+wayne-06@users.noreply.github.com>
2023-11-15 16:17:56 +0800

    fix: dashboard amount

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -2,11 +2,13 @@
 import AuthenticatedLayout from '@/Layouts/Authenticated.vue'
 import Button from '@/Components/Button.vue'
 import { GithubIcon } from '@/Components/Icons/brands'
+import {transactionFormat} from "@/Composables/index.js";
 
 defineProps({
     totalDeposit: [String, Number],
     totalWithdrawal: [String, Number],
 })
+const { formatAmount } = transactionFormat();
 
 </script>
 
@@ -26,14 +28,14 @@ defineProps({
                 class="border-2 border-[#05C46B] p-4 rounded-lg shadow-md text-center bg-gradient-to-b from-transparent to-[#05c46b33]"
             >
                 <legend class="text-xl px-4 uppercase font-semibold">{{ $t('public.Total Deposit') }} {{ $page.totalApprovedDeposit}}</legend>
-                <p class="text-4xl">$ {{ totalDeposit }}</p>
+                <p class="text-4xl">$ {{ formatAmount(totalDeposit) }}</p>
             </fieldset>
 
             <fieldset
                 class="border-2 border-[#FF3F34] p-4 rounded-lg shadow-md text-center bg-gradient-to-b from-transparent to-[#ff3f3433]"
             >
                 <legend class="text-xl px-4 uppercase font-semibold">{{ $t('public.Total Withdrawal') }}</legend>
-                <p class="text-4xl">$ {{ totalWithdrawal }}</p>
+                <p class="text-4xl">$ {{ formatAmount(totalWithdrawal) }}</p>
             </fieldset>
 
         </div>
